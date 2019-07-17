fb5b9125149f98d1007318cebccd062b884e4bb4
Victor Gabriel <victorsnt123@gmail.com>
2019-07-16 22:51:50 -0300

    ajuste

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { Container, Box, withStyles, TextField } from "@material-ui/core";
+import { Container, Box, withStyles, TextField, Select } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+
+import {rest} from '../authentication/tokenConfig'
 
 const useStyle = theme => ({
     root: {
         flexGrow: 1
     },
     box: {
-        marginLeft: "100px"
+        marginLeft: "10%",
+        marginRight: "10%",
+        marginBottom: "10%"
     },
     div: {
         width: "80%"
     },
     textField: {
         marginTop: theme.spacing(2),
+        width: "80%"
     }
 });
 class UserForm extends React.Component {
+    componentDidMount(){
+        rest("").post("/cargos").then(response => {
+            this.cargo = response.data;
+        })
+    }
     renderInput(campo) {
         console.log(campo);
         return (
@@ -28,6 +40,32 @@ class UserForm extends React.Component {
                 {...campo.input}
                 {...campo}
             />)
+    }
+
+    renderSelectField = ({
+        input,
+        label,
+        meta: { touched, error },
+        children,
+        ...custom
+      }) => {
+      console.log(custom)
+        return (
+            <FormControl error={touched && error}>
+                <InputLabel htmlFor="age-native-simple">Age</InputLabel>
+                    <Select
+                        native
+                        {...input}
+                        {...custom}
+                        inputProps={{
+                        name: 'age',
+                        id: 'age-native-simple'
+                        }}
+                    >
+                    {children}
+                </Select>
+            </FormControl>
+        )
     }
     render() {
         const { classes } = this.props;
@@ -91,6 +129,19 @@ class UserForm extends React.Component {
                                         className={classes.textField}
                                         type="text"
                                         />
+                                </Grid>
+                                <Grid item xs={6}>
+                                <Field
+                                    classes={classes}
+                                    name="responsavel.cargo"
+                                    component={this.renderSelectField}
+                                    label="Cargo"
+                                    >
+                                        <option value="" />
+                                        <option value={'ff0000'}>Red</option>
+                                        <option value={'00ff00'}>Green</option>
+                                        <option value={'0000ff'}>Blue</option>
+                                    </Field>
                                 </Grid>
                             </Grid>
                         </Box>
